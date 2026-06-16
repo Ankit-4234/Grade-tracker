4b3ad12a28d9139c24c503c71c9cce381774b5fe
Ankit Wosti <ankitwosti2@gmail.com>
2026-06-16 14:25:51 +0000

    starting index

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include "db.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>student grade tracker</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1> Student Grade Tracker</h1>
+    <a href="add.php">
+        <button class="btn add"> Add students </button>
+</a>
+<table>
+    
+</body>
+</html>
