a94d7c12ee71ba9896de1acd0d9ec49927b19a18
Ankit Wosti <ankitwosti2@gmail.com>
2026-06-21 16:55:42 +0000

    error solving and delete part

--- edit.php
+++ edit.php
@@ -2,19 +2,19 @@
 include "db.php";
  $id= $_GET['id'];
  $result= mysqli_query($conn, "SELECT * FROM students where id =$id");
- $row=my_sqli_fetch_assoc($result);
+ $row=mysqli_fetch_assoc($result);
  if(isset($_POST['update'])){
-    name= $_POST['name'];
-    math= $_POST['math'];
-    science= $_POST['science'];
-    english= $_POST['english'];
-    $sql= "UPDATE student SET
+    $name= $_POST['name'];
+    $math= $_POST['math'];
+    $science= $_POST['science'];
+    $english= $_POST['english'];
+    $sql= "UPDATE students SET
     name='$name',
     math= '$math',
-    science= '$science'
+    science= '$science',
     english= '$english'
     WHERE id =$id";
-    mqsql_query($conn,$sql);
+    mqsqli_query($conn,$sql);
     header("location:index.php");
  }
  ?>
@@ -30,10 +30,10 @@ include "db.php";
     <div class="container">
         <h1> Edit students </h1>
         <form method="POST">
-            <input type="text" name="name" value="<php echo $row['name']; ?>" required>
-            <input type="math" name="math" value="<php echo $row['math']; ?>" required>
-            <input type="science" name="science" value="<php echo $row['science']; ?>" required>
-            <input type="english" name="english" value="<php echo $row['english']; ?>" required>
+            <input type="text" name="name" value="<?php echo $row['name']; ?>" required>
+            <input type="math" name="math" value="<?php echo $row['math']; ?>" required>
+            <input type="science" name="science" value="<?php echo $row['science']; ?>" required>
+            <input type="english" name="english" value="<?php echo $row['english']; ?>" required>
             <button type="submit" name="update"> update student </button>
 </form>
 </div>
